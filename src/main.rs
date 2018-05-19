e709848f719a2feeb7f82e911a839eb7f2b480c3
Richard Dallaway <richard@dallaway.com>
2018-05-19 16:31:07 +0100

    first part, up to but not included tests

---
+++ src/main.rs
@@ -0,0 +1,19 @@
+extern crate minigrep;
+
+use std::env;
+use std::process;
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+
+    let config = minigrep::Config::new(&args).unwrap_or_else(|err| {
+        println!("Problem parsing arguments: {}", err);
+        process::exit(1);
+    });
+
+    println!("Searching for '{}' in '{}'", config.query, config.filename);
+    if let Err(e) = minigrep::run(config) {
+        println!("Application error: {}", e);
+        process::exit(1);
+    }
+}
